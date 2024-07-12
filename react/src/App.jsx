6e1c36390ba3f3f096a0ebb90a5f3d391bb9a315
wasadmin <wasadmin@example.com>
2024-07-12 13:43:53 -0400

    created component files

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// React imports
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+// import { AuthProvider } from "./hooks/AuthContext";
+import React, { useState, useEffect } from "react";
+
+// // Components
+// import LoginForm from "./components/LoginForm";
+// import WelcomePage from "./components/WelcomePage";
+// import Search from "./components/Search";
+// import SearchResults from "./components/SearchResults";
+// import EmployeeResult from "./components/EmployeeResult";
+
+
 import './App.css'
 
 function App() {
 
   return (
-    <div>Searchable Enterprise Directory</div>
+    <h1>Searchable Enterprise Directory</h1>
   )
 }
 
